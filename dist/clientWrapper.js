19fe22b200101ff83c3b920ae89180592b5dea3f
Guido Lorenz <guido.lorenz@artcom.de>
2015-09-07 12:36:16 +0200

    Move ClientWrapper into separate file

---
+++ dist/clientWrapper.js
@@ -0,0 +1,86 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var _helpers = require("./helpers");
+
+var ClientWrapper = (function () {
+  function ClientWrapper(client) {
+    _classCallCheck(this, ClientWrapper);
+
+    this.client = client;
+    this.subscriptions = {};
+
+    this.client.on("message", this.handleMessage.bind(this));
+  }
+
+  _createClass(ClientWrapper, [{
+    key: "publish",
+    value: function publish(topic, payload) {
+      var _this = this;
+
+      return new Promise(function (resolve, reject) {
+        var retain = !(0, _helpers.isEventOrCommand)(topic);
+        _this.client.publish(topic, JSON.stringify(payload), { retain: retain, qos: 2 }, resolve);
+      });
+    }
+  }, {
+    key: "subscribe",
+    value: function subscribe(topic, handler) {
+      var _this2 = this;
+
+      return new Promise(function (resolve, reject) {
+        var regexp = (0, _helpers.topicRegexp)(topic);
+        var subscribe = false;
+
+        if (!_this2.subscriptions[topic]) {
+          _this2.subscriptions[topic] = [];
+          subscribe = true;
+        }
+
+        _this2.subscriptions[topic].push({ handler: handler, regexp: regexp });
+
+        if (subscribe) {
+          _this2.client.subscribe(topic, resolve);
+        } else {
+          resolve();
+        }
+      });
+    }
+  }, {
+    key: "handleMessage",
+    value: function handleMessage(topic, json, packet) {
+      var _this3 = this;
+
+      try {
+        (function () {
+          var payload = JSON.parse(json);
+
+          Object.keys(_this3.subscriptions).forEach(function (key) {
+            _this3.subscriptions[key].forEach(function (_ref) {
+              var handler = _ref.handler;
+              var regexp = _ref.regexp;
+
+              if (regexp.test(topic)) {
+                handler(payload, topic, packet);
+              }
+            });
+          });
+        })();
+      } catch (error) {
+        // ignore exceptions during JSON parsing
+      }
+    }
+  }]);
+
+  return ClientWrapper;
+})();
+
+exports["default"] = ClientWrapper;
+module.exports = exports["default"];
--- src/topping.js
+++ src/topping.js
@@ -1,58 +1,6 @@
 import mqtt from "mqtt";
 
-import {isEventOrCommand, topicRegexp} from "./helpers";
-
-class ClientWrapper {
-  constructor(client) {
-    this.client = client;
-    this.subscriptions = {};
-
-    this.client.on("message", this.handleMessage.bind(this));
-  }
-
-  publish(topic, payload) {
-    return new Promise((resolve, reject) => {
-      const retain = !isEventOrCommand(topic);
-      this.client.publish(topic, JSON.stringify(payload), { retain: retain, qos: 2 }, resolve);
-    });
-  }
-
-  subscribe(topic, handler) {
-    return new Promise((resolve, reject) => {
-      const regexp = topicRegexp(topic);
-      let subscribe = false;
-
-      if (!this.subscriptions[topic]) {
-        this.subscriptions[topic] = [];
-        subscribe = true;
-      }
-
-      this.subscriptions[topic].push({handler, regexp});
-
-      if (subscribe) {
-        this.client.subscribe(topic, resolve);
-      } else {
-        resolve();
-      }
-    });
-  };
-
-  handleMessage(topic, json, packet) {
-    try {
-      const payload = JSON.parse(json);
-
-      Object.keys(this.subscriptions).forEach((key) => {
-        this.subscriptions[key].forEach(({handler, regexp}) => {
-          if (regexp.test(topic)) {
-            handler(payload, topic, packet);
-          }
-        });
-      });
-    } catch (error) {
-      // ignore exceptions during JSON parsing
-    }
-  }
-}
+import ClientWrapper from "./clientWrapper";
 
 module.exports = {
   connect: function(uri) {
