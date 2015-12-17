272f8a90b14c25d57b316fe6126f0fb716a11971
Guido Lorenz <guido.lorenz@artcom.de>
2015-12-17 15:45:48 +0100

    Make topic regexp test failures more telling

--- test/helpers.js
+++ test/helpers.js
@@ -19,44 +19,44 @@ describe("Helpers", function() {
   it("should create simple topic regexp", function() {
     const re = topicRegexp("foo/bar/baz")
 
-    expect(re.test("foo/bar/baz")).to.be.true
+    expect("foo/bar/baz").to.match(re)
 
-    expect(re.test("foo/bar/bazinga")).to.be.false
-    expect(re.test("foo/bar/ba")).to.be.false
-    expect(re.test("foo/bar/.*")).to.be.false
+    expect("foo/bar/bazinga").not.to.match(re)
+    expect("foo/bar/ba").not.to.match(re)
+    expect("foo/bar/.*").not.to.match(re)
   })
 
   it("should create wildcard topic regexp with plus", function() {
     const re = topicRegexp("foo/+/baz")
 
-    expect(re.test("foo/bar/baz")).to.be.true
-    expect(re.test("foo/lala/baz")).to.be.true
+    expect("foo/bar/baz").to.match(re)
+    expect("foo/lala/baz").to.match(re)
 
-    expect(re.test("foo/lala/bazinga")).to.be.false
-    expect(re.test("foo/lala/ba")).to.be.false
+    expect("foo/lala/bazinga").not.to.match(re)
+    expect("foo/lala/ba").not.to.match(re)
   })
 
   it("should create wildcard topic regexp with trailing plus", function() {
     const re = topicRegexp("foo/bar/+")
 
-    expect(re.test("foo/bar/baz")).to.be.true
-    expect(re.test("foo/bar/bazinga")).to.be.true
+    expect("foo/bar/baz").to.match(re)
+    expect("foo/bar/bazinga").to.match(re)
 
-    expect(re.test("foo/bar")).to.be.false
-    expect(re.test("foo/bar/ba/zin/ga")).to.be.false
-    expect(re.test("foo/lala/baz")).to.be.false
+    expect("foo/bar").not.to.match(re)
+    expect("foo/bar/ba/zin/ga").not.to.match(re)
+    expect("foo/lala/baz").not.to.match(re)
   })
 
   it("should create wildcard topic regexp with hash", function() {
     const re = topicRegexp("foo/bar/#")
 
-    expect(re.test("foo/bar/baz")).to.be.true
-    expect(re.test("foo/bar/bazinga")).to.be.true
-    expect(re.test("foo/bar/ba")).to.be.true
-    expect(re.test("foo/bar/ba/zin/ga")).to.be.true
-    expect(re.test("foo/bar")).to.be.true
+    expect("foo/bar/baz").to.match(re)
+    expect("foo/bar/bazinga").to.match(re)
+    expect("foo/bar/ba").to.match(re)
+    expect("foo/bar/ba/zin/ga").to.match(re)
+    expect("foo/bar").to.match(re)
 
-    expect(re.test("foo/barista")).to.be.false
-    expect(re.test("foo/lala")).to.be.false
+    expect("foo/barista").not.to.match(re)
+    expect("foo/lala").not.to.match(re)
   })
 })
