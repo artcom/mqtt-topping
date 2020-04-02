509605611527667b5755ccbf7c04e5daed332663
Mirko Geissler <mirko.geissler@artcom.de>
2020-04-02 08:31:19 +0200

    Set keepalive and connectTimeout

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,15 @@ import { ClientOptions } from "./mqtt/types"
 export { default as HttpClient } from "./http/httpClient"
 export { unpublishRecursively } from "./utils"
 
-export async function connectMqttClient(uri: string, options?: ClientOptions) {
-  const client = await connectAsync(uri, options, false)
-  return new MqttClient(client, options?.onParseError)
+export async function connectMqttClient(uri: string, options: ClientOptions = {}) {
+  const { onParseError, ...rest } = options
+  const clientOptions = {
+    keepalive: 3,
+    connectTimeout: 3000,
+    ...rest
+  }
+
+  const client = await connectAsync(uri, clientOptions, false)
+  return new MqttClient(client, onParseError)
 }
 
