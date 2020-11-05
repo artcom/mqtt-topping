d7624f1544d100633982ea642e3c19bd1692178a
Mirko Geissler <mirko.geissler@artcom.de>
2020-11-05 13:01:08 +0100

    Cleanup unpublishRecursively

--- src/utils.ts
+++ src/utils.ts
@@ -6,12 +6,12 @@ import { default as HttpClient } from "./http/httpClient"
 export async function unpublishRecursively(
   mqttClient: MqttClient, httpClient: HttpClient, topic: string
 ) : Promise<IPublishPacket[]> {
-  const result = await httpClient.query({ topic, depth: -1, flatten: true, parseJson: false })
-  const subTopics = result as FlatTopicResult[]
-  const unpublishPromises = subTopics.reduce<Promise<IPublishPacket>[]>(
-    (promises, subTopic) =>
-      subTopic.payload ? [...promises, mqttClient.unpublish(subTopic.topic)] : promises,
-    [])
+  const query = { topic, depth: -1, flatten: true, parseJson: false }
+  const subTopics = (await httpClient.query(query)) as FlatTopicResult[]
 
-  return Promise.all(unpublishPromises)
+  const promises = subTopics
+    .filter(subTopic => subTopic.payload !== undefined)
+    .map(subTopic => mqttClient.unpublish(subTopic.topic))
+
+  return Promise.all(promises)
 }
